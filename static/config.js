a9f955a901d6ec4ea93a3cd7fc51f1e62c87cbe2
frank <liangzhenqi@hotmail.com>
2019-06-30 21:03:22 +0800

    add car_demo

---
+++ static/config.js
@@ -0,0 +1,7 @@
+window.g={
+  MAP_URL: 'https://webst02.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
+  query_URL:'http://192.168.0.136:8080/',
+  queryAllDevice_URL: './API/queryAllDevice.json',
+  queryTrack_URL: 'http://192.168.0.136:8080/mobileye/queryTrack',
+  queryCarPosition_URL: 'http://192.168.0.136:8080/mobileye/queryCarPosition'
+}
